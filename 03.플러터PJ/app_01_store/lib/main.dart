153cc44c3851812368a4fb168c93246f5ce21703
Choisj01 <choisujin96@naver.com>
2023-12-26 16:42:33 +0900

    Update main.dart

--- 03.플러터PJ/app_01_store/lib/main.dart
+++ 03.플러터PJ/app_01_store/lib/main.dart
@@ -13,13 +13,12 @@ class MyApp extends StatelessWidget {
     // 최상위 위젯 : 전체 틀 잡아줌!
     return MaterialApp(
       // "DEBUG" 표시 없애기
-       debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: false,
       // home에 작성하는 위젯이 실제 이 앱이 표시하는 위젯
       home: StorePage(), // 이 클래스를 아래쪽에 생성함!
     );
   }
 }
-
 
 // StorePage 클래스 만들기!!!////////
 // -> 비상태위젯 단축키 : stl
@@ -44,27 +43,48 @@ class StorePage extends StatelessWidget {
             // Row - 가로방향 위젯요소 넣기
             Padding(
               padding: const EdgeInsets.all(25.0),
-              child: Row(children: [
-                Text("Woman",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,fontSize: 20.0,color: Colors.red),),
-                Spacer(),// 사이공간 일정하게 띄우기
-                Text("Kids",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,fontSize: 20.0,color: Colors.orange),),
-                Spacer(),// 사이공간 일정하게 띄우기
-                Text("Shoes",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,fontSize: 20.0,color: Colors.yellow),),
-                Spacer(),// 사이공간 일정하게 띄우기
-                Text("Bag",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,fontSize: 20.0,color: Colors.green),),
-              ],),
-            )
+              child: Row(
+                children: [
+                  Text(
+                    "Woman",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20.0,
+                        color: Colors.red),
+                  ),
+                  Spacer(), // 사이공간 일정하게 띄우기
+                  Text(
+                    "Kids",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20.0,
+                        color: Colors.orange),
+                  ),
+                  Spacer(), // 사이공간 일정하게 띄우기
+                  Text(
+                    "Shoes",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20.0,
+                        color: Colors.yellow),
+                  ),
+                  Spacer(), // 사이공간 일정하게 띄우기
+                  Text(
+                    "Bag",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20.0,
+                        color: Colors.green),
+                  ),
+                ],
+              ),
+            ),
+            // 이미지 추가하기(패딩으로 싼 첫번째 줄 아래)
+            Image.asset("assets/back.jpeg"),
+            Image.asset("assets/cloth.jpeg"),
           ],
-        ),),
+        ),
+      ),
     );
   }
 }
-
