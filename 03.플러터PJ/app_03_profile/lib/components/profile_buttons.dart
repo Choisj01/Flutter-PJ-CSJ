2e2d71cdfd7fc39741f8f529d57654cbbb4f122c
Choisj01 <choisujin96@naver.com>
2024-01-04 10:20:23 +0900

    Create profile_buttons.dart

---
+++ 03.플러터PJ/app_03_profile/lib/components/profile_buttons.dart
@@ -0,0 +1,13 @@
+// 프로파일 버튼 클래스
+
+import 'package:flutter/material.dart';
+
+class ProfileButtons extends StatelessWidget {
+  const ProfileButtons({Key? key}) : super(key: key);
+
+  // 앱 빌드 재정의!
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
