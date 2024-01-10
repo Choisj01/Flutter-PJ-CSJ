d575fabfc0fda3b3c81b4c925473089050b43424
Choisj01 <choisujin96@naver.com>
2024-01-10 11:36:41 +0900

    ㅇ

--- 03.플러터PJ/app_04_login/lib/main.dart
+++ 03.플러터PJ/app_04_login/lib/main.dart
@@ -4,34 +4,28 @@ void main() {
   runApp(const MyApp());
 }
 
+/* 
+  [ 로그인 앱 전체구성 : lib폴더하위 ]
+  1. 메인 출발(추가 사이즈 지정  size.dart)
+    ->>> main.dart에 라우터 속성추가!!!
+  2. 페이지 구성 : 2개
+    pages/home_page.dart : 홈페이지 
+    pages/login_page.dart : 로그인 페이지
+  3. 컴포넌트 구성 : 3개
+    components/logo.dart : SVG로고
+    components/custom_form.dart : Form 위젯
+    components/custom_text_form_field.dart : Form 요소 위젯
+ */
+
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
