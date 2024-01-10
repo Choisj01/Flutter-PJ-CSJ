a6712aa116d3059cb1540868df4a412bc5ee434e
Choisj01 <choisujin96@naver.com>
2024-01-10 12:58:35 +0900

    Update home_page.dart

---
+++ 03.플러터PJ/app_04_login/lib/pages/home_page.dart
@@ -0,0 +1,17 @@
+// 홈 페이지 //////////////////
+
+import 'package:flutter/material.dart';
+
+// StatelessWidget 불러오는 단축키 : stl
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       body: Container(
+        color: Colors.blue,
+      ),
+    );
+  }
+}
