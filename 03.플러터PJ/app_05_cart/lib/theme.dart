b6a06ce430f61098a007cd9e07e8084f4ad69ec9
Choisj01 <choisujin96@naver.com>
2024-01-16 10:36:38 +0900

    플러터PJ_5

---
+++ 03.플러터PJ/app_05_cart/lib/theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:app_05_cart/constants.dart';
+
+ThemeData theme() {
+  return ThemeData(
+    primarySwatch: kPrimaryColor,
+    scaffoldBackgroundColor: kPrimaryColor,
+  );
+}
